ce1a959b486bf6e82bbee028e19e229769e88c6b
Nehasridhara <95270534+Nehasridhara@users.noreply.github.com>
2022-03-05 08:21:59 +0000

    done

--- p1final.c
+++ p1final.c
@@ -14,11 +14,11 @@ void input(int *num1, int *den1, int *num2, int *den2)
 int find_gcd(int a,int b)
 {
   int gcd;
-  for(int i =i; i<=a && i<=b; ++i)
+  for(int i = 1; i<=a && i<=b; ++i)
   {
-    if(a % i == 0 && b% i == 0)
+    if(a % i == 0 && b % i == 0)
     {
-      gcd = 1;
+      gcd = i;
     }
   }
   return gcd;
