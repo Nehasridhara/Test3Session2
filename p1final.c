61b41fba79eaa17ff20d52719357d28fe7800aed
Nehasridhara <95270534+Nehasridhara@users.noreply.github.com>
2022-03-05 06:51:45 +0000

    done

--- p1final.c
+++ p1final.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 void input(int *num1, int *den1, int *num2, int *den2)
 {
-  printf("enter the numerator:\n");
+  printf("enter the first fraction(num):\n");
   scanf("%d",num1);
-  printf("enter the denominator:\n");
+  printf("enter the first fraction(den):\n");
   scanf("%d",den1);
-   printf("enter the numerator:\n");
+   printf("enter the second fraction(num):\n");
   scanf("%d",num2);
-  printf("enter the denominator:\n");
+  printf("enter the second fraction(den):\n");
   scanf("%d",den2);
+}
+
+int find_gcd(int a,int b)
+{
+  int gcd;
+  for(int i =i; i<=a && i<=b; ++i)
+  {
+    if(a % i == 0 && b% i == 0)
+    {
+      gcd = 1;
+    }
+  }
+  return gcd;
 }
 
 void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
 {
-  *num3=(num1*den2)+(num2*den1);
-  *den3=den1*den2;
+  int a,b,gcd;
+  a=(num1*den2)+(num2*den1);
+  b=den1*den2;
+  gcd=find_gcd(a,b);
+  *num3=a/gcd;
+  *den3=b/gcd;
 }
 
-void output(int num3, int den3)
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
 {
-  printf("the sum of two fractions is %d/%d",num3,den3);
+  printf(" %d/%d + %d/%d = %d/%d :\n",num1,den1,num2,den2,num3,den3);
 }
 
 int main()
@@ -27,6 +44,6 @@ int main()
   int num1,den1,num2,den2,num3,den3;
   input(&num1,&den1,&num2,&den2);
   add(num1,den1,num2,den2,&num3,&den3);
-  output(num3,den3);
+  output(num1,den1,num2,den2,num3,den3);
   return 0;
 }
