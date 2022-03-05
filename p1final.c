9e41aad6361e675a2cb69f081de94e491bc73804
Nehasridhara <95270534+Nehasridhara@users.noreply.github.com>
2022-03-05 05:58:04 +0000

    done

--- p1final.c
+++ p1final.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("enter the numerator:\n");
+  scanf("%d",num1);
+  printf("enter the denominator:\n");
+  scanf("%d",den1);
+   printf("enter the numerator:\n");
+  scanf("%d",num2);
+  printf("enter the denominator:\n");
+  scanf("%d",den2);
+}
+
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  *num3=(num1*den2)+(num2*den1);
+  *den3=den1*den2;
+}
+
+void output(int num3, int den3)
+{
+  printf("the sum of two fractions is %d/%d",num3,den3);
+}
+
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num3,den3);
+  return 0;
+}
